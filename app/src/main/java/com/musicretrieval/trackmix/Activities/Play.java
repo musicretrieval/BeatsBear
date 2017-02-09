364fec9fd8652a302da9cc2d19d4a19a7ffd3859
james-woo <jamesjaywoo@gmail.com>
2017-02-08 18:33:41 -0800

    Add dsp library

--- app/src/main/java/com/musicretrieval/trackmix/Activities/Play.java
+++ app/src/main/java/com/musicretrieval/trackmix/Activities/Play.java
@@ -6,18 +6,59 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.musicretrieval.trackmix.R;
 
+import java.util.ArrayList;
+
+import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.AudioEvent;
+import be.tarsos.dsp.AudioProcessor;
+import be.tarsos.dsp.io.android.AudioDispatcherFactory;
+import be.tarsos.dsp.pitch.PitchDetectionHandler;
+import be.tarsos.dsp.pitch.PitchDetectionResult;
+import be.tarsos.dsp.pitch.PitchProcessor;
 import butterknife.ButterKnife;
 
 public class Play extends AppCompatActivity {
+
+    private ArrayList<String> songs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play);
         ButterKnife.bind(this);
+
+        songs = getIntent().getStringArrayListExtra("PLAY_PLAYLIST");
+
+        play(songs.get(0));
+    }
+
+    public void play(String song) {
+        final int sampleRate = 44100;
+        final int bufferSize = 4096;
+
+        AudioDispatcher dispatcher = AudioDispatcherFactory.fromPipe(song, sampleRate, bufferSize, 0);
+
+        PitchDetectionHandler pdh = new PitchDetectionHandler() {
+            @Override
+            public void handlePitch(PitchDetectionResult pitchDetectionResult, AudioEvent audioEvent) {
+                final float pitchInHz = pitchDetectionResult.getPitch();
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        TextView tv = (TextView) findViewById(R.id.play_test);
+                        tv.setText("" + pitchInHz);
+                    }
+                });
+            }
+        };
+
+        AudioProcessor p = new PitchProcessor(PitchProcessor.PitchEstimationAlgorithm.FFT_YIN, sampleRate, bufferSize, pdh);
+        dispatcher.addAudioProcessor(p);
+        new Thread(dispatcher, "Audio Dispatcher").start();
     }
 
 }
